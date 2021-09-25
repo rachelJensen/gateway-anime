6a7f9d37642d49b73f70ed361286dfb432317174
Rachel Jensen <rachelangela@gmail.com>
2021-09-25 14:32:20 -0600

    Add basic header styling

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -1,7 +1,14 @@
 import './NavBar.css';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
-  return (<p className="navbar">NavBar</p>)
+  return (
+  <nav>
+    <Link to="/" className="link" >Home</Link>
+    <Link to="/about"className="link" >About</Link>
+    <Link to="/to-watch" className="link" >To Watch</Link>
+  </nav>
+  )
 }
 
 export default NavBar;
